4c75b9c57173bb679f60434688b83313ee3d6dc0
RAJESHWAR-CHOUDHARY <rajeshwarchoudhary053@gmail.com>
2025-04-15 18:06:16 +0530

    Add input handling and arithmetic operations in Java classes

---
+++ sec.java
@@ -0,0 +1,12 @@
+public class sec {
+    public static void main(String[] args){
+    System.out.print("The Sum of these number is :"); //print can also be used here normal print is used to print  output in same line 
+    int num1 = 6;
+    int num2 = 7;
+    int num3 = 5;
+    int sum =num1+num2+num3;
+    System.out.print(sum); // same line
+    System.out.println(sum); // print in same line after this output will be in new line 
+    }
+}
+
