aa2f4f3b42fc4450fa3d0118e25dc9a5d137bc82
RAJESHWAR-CHOUDHARY <rajeshwarchoudhary053@gmail.com>
2025-04-15 18:05:54 +0530

    Implement input handling and arithmetic operations in Java classes

---
+++ lieralss.java
@@ -0,0 +1,14 @@
+public class lieralss {
+    public static void main(String[] args){
+        byte age = 14;
+        char ch = 'd';
+        float f1 = 5.88f;
+        int age2 = 55;
+        double f2 = 5.10;
+        long agedno = 544444444444444l;
+        boolean a = false;
+        String B ="rajeshwar";
+        System.out.println(age);
+        System.out.println(B);
+    }
+}
