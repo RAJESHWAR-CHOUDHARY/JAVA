964d4bde930ab48e72f3619b79082406090c9ae5
RAJESHWAR-CHOUDHARY <rajeshwarchoudhary053@gmail.com>
2025-04-15 18:07:38 +0530

    TAKING USER INPUT ARITHMATIC

---
+++ input.java
@@ -0,0 +1,39 @@
+// import java.util.Scanner;
+// public class input {
+//     public static void main(String[] args){
+    // System.out.println("taking inputs for user");
+    // Scanner sc = new Scanner(System.in); // system.in take input from keyboard 
+    //sc can be any name its a variable
+    // System.out.println("enter first no");
+    // int a = sc.nextInt(); 
+    // System.out.println("enter 2nd no.");
+    // int b = sc.nextInt();
+    // boolean b1 = sc.hasNextInt();
+    // System.out.println(b1);
+    // int sum =a+b;
+    // System.out.print("the sum of these number is  : ");
+    // System.out.println(sum);
+//     String str =sc.next();
+//     System.out.println(str);
+//     }
+// }
+
+
+
+import java.util.Scanner;
+public class input{
+    public static void main(String[] args){
+        System.out.println("taking input  from use");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter 1st no.");
+        int a = sc.nextInt();
+        System.out.println("enter 2st no.");
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.print(sum);
+    }
+}
+
+
+
+
